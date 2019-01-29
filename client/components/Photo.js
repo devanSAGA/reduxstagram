44bc04679d28f511d974ba42961b59f8ace2c9f2
Devansh Purohit <officialdevansh@gmail.com>
2019-01-29 18:44:29 +0530

    configured the like button

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -11,6 +11,23 @@ class Photo extends React.Component {
             <img src={post.display_src} alt={post.caption} />
           </Link>
         </div>
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button
+              className="likes"
+              onClick={() => this.props.increment(index)}
+            >
+              &hearts; {post.likes}
+            </button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble" />
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
       </figure>
     );
   }
